f93421523179437648ec75932c55892a58830bbc
Adam <33823436+DerpyDinosaur@users.noreply.github.com>
2019-07-27 15:31:16 +0800

    First and Final

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Bank Login">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<title>Universal Bank Login</title>
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<link rel="stylesheet" href="fontawesome/css/all.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>The Universal Bank</h1>
+	</header>
+	<main>
+		<h2 class="loginHeader">Login Page</h2>
+		<form>
+			<h4>Username</h4>
+			<input class="form-control" type="text" name="username">
+			<h4>Password</h4>
+			<input class="form-control" type="text" name="password">
+			<button class='btn btn-primary' type='submit' name='loginBtn'>Sign Up</button>
+		</form>
+	</main>
+	<footer>
+		
+	</footer>
+</body>
+</html>
